2f2bb93e7f8187ddb9faa367ecd4bac7dd81fbef
RyotoTsuchiyama <106653745+ttymryo@users.noreply.github.com>
2022-08-02 15:19:03 +0000

    [Add]enum

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -2,4 +2,8 @@ class Diary < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  enum public_renge: { myself: 0, in_ff: 1, everyone: 2 }
+  enum emotion: { happy: 0, normal: 1, unhappy: 2 }
+
 end
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module KyouyuNikk
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config/locals/*.yml').to_s]
   end
 end
