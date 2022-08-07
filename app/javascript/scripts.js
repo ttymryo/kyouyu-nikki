e48fe6c45b29b124076231e18547b1d6b4565f45
RyotoTsuchiyama <106653745+ttymryo@users.noreply.github.com>
2022-08-07 05:59:55 +0000

    [Add]loading effect

--- app/javascript/scripts.js
+++ app/javascript/scripts.js
@@ -3,7 +3,7 @@ $(document).on('turbolinks:load', function() {
     contentSelector: '.jscroll-div',
     autoTrigger: true,
     nextSelector: '.next a',  // 次ページリンクのセレクタ
-    loadingHtml: '<h4 class="text-center p-5">読み込み中</h4>',
+    loadingHtml: '<div class="sk-fading-circle"><div class="sk-circle1 sk-circle"></div><div class="sk-circle2 sk-circle"></div><div class="sk-circle3 sk-circle"></div><div class="sk-circle4 sk-circle"></div><div class="sk-circle5 sk-circle"></div><div class="sk-circle6 sk-circle"></div><div class="sk-circle7 sk-circle"></div><div class="sk-circle8 sk-circle"></div><div class="sk-circle9 sk-circle"></div><div class="sk-circle10 sk-circle"></div><div class="sk-circle11 sk-circle"></div><div class="sk-circle12 sk-circle"></div></div>',
     padding: 10
   });
 });
