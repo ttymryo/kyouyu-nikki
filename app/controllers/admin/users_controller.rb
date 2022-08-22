2bebdc43ca2144ccc51efcfdfa30f6a7dc0e813d
RyotoTsuchiyama <106653745+ttymryo@users.noreply.github.com>
2022-08-22 05:32:00 +0000

    [Add]user is_deleted

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,4 +3,28 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all.page(params[:page]).per(20)
   end
+
+  def show
+    @user = User.find(params[:id])
+    @diaries = @user.diaries.order(created_at: :desc).page(params[:page]).per(10)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(params_user)
+      redirect_to admin_user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def params_user
+    params.require(:user).permit(:name,:name_id,:email,:image,:introduction,:is_public,:is_deleted)
+  end
 end
