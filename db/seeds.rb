55217465e31becaa35777580d32e28296d571367
RyotoTsuchiyama <106653745+ttymryo@users.noreply.github.com>
2022-08-24 08:10:42 +0000

    [Update]seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,11 @@
 Admin.create(email: 'admin@admin',password: 'minminadminmin')
 
 User.create(name: 'テストユーザー', name_id: 'testuser',email: 'test@test',password: 'usertesttestuser')
+User.create(name: '凍結ユーザー', name_id: 'touketuuser',email: 'touketu@test',password: 'touketu',is_public: false,is_deleted: true)
 
-Diary.create(user_id: 1, body: 'てすと！', emotion: 0, public_range: 2, add_commented: true)
+Diary.create(user_id: 1, body: 'てすと！全員コメントおｋ', emotion: 0, public_range: 2, add_commented: true)
+Diary.create(user_id: 1, body: 'てすと！全員コメントだめ', emotion: 0, public_range: 2, add_commented: false)
+Diary.create(user_id: 1, body: 'てすと！FFコメントだめ', emotion: 0, public_range: 1, add_commented: false)
+Diary.create(user_id: 1, body: 'てすと！自分コメントだめ', emotion: 0, public_range: 0, add_commented: false)
+
+Diary.create(user_id: 2, body: 'てすと！全員コメントおｋ', emotion: 0, public_range: 2, add_commented: true)
