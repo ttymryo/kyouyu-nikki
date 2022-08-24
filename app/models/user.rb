42cba9de038f0fef25f9a451ec743c4c0c2fb096
RyotoTsuchiyama <106653745+ttymryo@users.noreply.github.com>
2022-08-24 09:04:54 +0000

    [Fix]guestuser [Add]pry-rails

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,9 +51,14 @@ class User < ApplicationRecord
   end
 
   def self.guest
-    find_or_create_by!(name: 'ゲストユーザー' ,name_id: 'guestuser',introduction: 'このアカウントは共用アカウントです。すべての機能が利用できます。※プロフィールの設定は保存されません。' ,email: 'guest@example.com') do |user|
+    find_or_create_by!(
+      name: 'ゲストユーザー' ,
+      name_id: 'user',
+      introduction: 'このアカウントは共用アカウントです。すべての機能が利用できます。プロフィールの設定は保存されません。' ,
+      email: 'user@user'
+      ) do |user|
       user.password = SecureRandom.urlsafe_base64
-      user.name_id = "guestuser"
+      user.name_id = "user"
     end
   end
 
