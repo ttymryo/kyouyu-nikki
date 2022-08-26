c58eddc39591a70a0b8d2897e53f1fb8c474d4aa
RyotoTsuchiyama <106653745+ttymryo@users.noreply.github.com>
2022-08-26 18:27:02 +0000

    [Update]layouts

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -14,7 +14,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
       keys: [
-        :name, :name_id, :email,
+        :name, :name_id, :email, :introduction
       ])
   end
 
