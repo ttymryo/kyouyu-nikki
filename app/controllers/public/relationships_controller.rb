b69492f4cac06eb3c713a5cc40c70243803d8f14
RyotoTsuchiyama <106653745+ttymryo@users.noreply.github.com>
2022-08-29 22:25:42 +0000

    [Update]layouts

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,15 +1,21 @@
 class Public::RelationshipsController < ApplicationController
   before_action :custom_authenticate
-  
+  before_action :myself?
+
   def create
-    user = User.find_by!(name_id: params[:user_name_id])
-    current_user.follow(user.id)
+    current_user.follow(@user.id)
     redirect_to request.referer, notice: 'フォローしました'
   end
 
   def destroy
-    user = User.find_by!(name_id: params[:user_name_id])
-    current_user.unfollow(user.id)
+    current_user.unfollow(@user.id)
     redirect_to request.referer, notice: 'フォローを解除しました。'
+  end
+
+  def myself?
+    @user = User.find_by!(name_id: params[:user_name_id])
+    if current_user == @user
+      redirect_to request.referer, alert: '自分はフォローできません'
+    end
   end
 end
