f0832e6a48a2de70d87ecd86d899ba53e1d1c319
RyotoTsuchiyama <106653745+ttymryo@users.noreply.github.com>
2022-08-30 15:41:58 +0000

    [Update]error message, html 制限

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,28 @@ module KyouyuNikk
     config.time_zone = 'Asia/Tokyo'
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locals/*.yml').to_s]
+    
+    #エラーメッセージ
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+
+      if instance.kind_of?(ActionView::Helpers::Tags::Label)
+
+        html_tag.html_safe
+
+      else
+
+        class_name = instance.instance_variable_get(:@object_name)
+
+        method_name = instance.instance_variable_get(:@method_name)
+
+        input_error_icon = html_tag.gsub("form-control", "form-control is-invalid").html_safe
+
+        below_error_message = "<div class=\"invalid-feedback\">#{I18n.t("activerecord.attributes.#{class_name}.#{method_name}")}#{instance.error_message.first}</div>".html_safe
+
+        input_error_icon.concat(below_error_message)
+
+      end
+
+    end
   end
 end
