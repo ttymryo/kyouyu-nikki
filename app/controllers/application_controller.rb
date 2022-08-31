d6de66e6baf48544609202faebe4335ad36f757e
RyotoTsuchiyama <106653745+ttymryo@users.noreply.github.com>
2022-08-31 15:12:33 +0000

    [Fix]facion routing error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
   #findなどで例外が発生した場合リダイレクトする
   rescue_from ActiveRecord::RecordNotFound do |exception|
-    redirect_to :root, alert: 'エラーが発生しました'
+    if params[:name_id] == 'favicon.icofavicon'
+      redirect_to root_path
+    else
+      redirect_to :root, alert: 'エラーが発生しました'
+    end
   end
 
 
